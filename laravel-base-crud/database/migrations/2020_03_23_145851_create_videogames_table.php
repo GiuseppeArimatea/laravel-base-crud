7d0d2da6a3e53c5f5dc2dd8a94cdc4c50115b047
giuseppe <Giuseppearimatea10@gmail.com>
2020-03-23 16:03:11 +0100

    commit

---
+++ laravel-base-crud/database/migrations/2020_03_23_145851_create_videogames_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVideogamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videogames', function (Blueprint $table) {
+            $table->id();
+            $table->string('titolo');
+            $table->string('Casa Produttrice');
+            $table->string('codice' 13);
+            $table->float('Prezzo' 6,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videogames');
+    }
+}
